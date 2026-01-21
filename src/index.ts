b7957521febab8e4c1d5e69078d9df29ab1253c3
izure <izure@naver.com>
2026-01-22 06:42:46 +0900

    feat: MVCC API 컴포넌트 내보내기를 위한 `index.ts` 파일을 추가하고 패키지 버전을 1.2.6으로 업데이트합니다.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 'use strict'
 
+export { TransactionEntry, TransactionConflict, TransactionMergeFailure, TransactionResult } from './types'
+export * from './core/base'
 export * from './core/sync'
 export * from './core/async'
